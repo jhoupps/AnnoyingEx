ad3dded4eef54780036c10bbbf6a96ff5e886436
jhoupps <j.c.houp7575@gmail.com>
2020-05-21 15:29:22 -0700

    all works except for fetching from the internet

--- app/src/main/java/com/jhoupps/annoyingex/ExNotificationManager.kt
+++ app/src/main/java/com/jhoupps/annoyingex/ExNotificationManager.kt
@@ -3,6 +3,7 @@ package com.jhoupps.annoyingex
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
+import android.app.TaskStackBuilder
 import android.content.Context
 import android.content.Intent
 import android.os.Build
@@ -22,23 +23,50 @@ class ExNotificationManager (
 
 
     fun postItNote() {
+        // Create an Intent for the activity you want to start
+        val mainIntent = Intent(context, MainActivity::class.java)
+        // Create the TaskStackBuilder
+        val mainPendingIntent: PendingIntent? = TaskStackBuilder.create(context).run {
+            // Add the intent, which inflates the back stack
+            addNextIntentWithParentStack(mainIntent)
+            // Get the PendingIntent containing the entire back stack
+            getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT)
+        }
+
+
+
+
         val notification = NotificationCompat.Builder(context, FUN_CHANNEL_ID)
             .setSmallIcon(R.drawable.ic_sentiment_very_dissatisfied_black_24dp)
             .setContentTitle("That F@#%ing B@st@rd")
             .setContentText(getMessage())
             .setCategory(NotificationCompat.CATEGORY_MESSAGE)
             .setPriority(NotificationCompat.PRIORITY_DEFAULT)
-          //  .setContentIntent(pendingDealsIntent)
+            .setContentIntent(mainPendingIntent)
             .setAutoCancel(true) //closes notification upon being tapped
-                //TODO - LAUNCH MAIN ACTIVITY ON TAP
             .build()
 
         notificationManagerCompat.notify(Random.nextInt(), notification)
     }
 
+    private fun createFunChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = "Fun Notifications"
+            val descriptionText = "All Msgs from a great autotune voiced dude"
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel(FUN_CHANNEL_ID, name, importance).apply {
+                description = descriptionText
+            }
+            notificationManagerCompat.createNotificationChannel(channel)
+        }
+    }
+
+
     //todo make changes here when i go online
     fun getMessage(): String {
-    //todo this should be from the internet
+        //todo this should be from the internet
         val messagebank = listOf<String>(
             "hey u up?",
             "wyd",
@@ -76,22 +104,12 @@ class ExNotificationManager (
 
     }
 
-    private fun createFunChannel() {
-        // Create the NotificationChannel, but only on API 26+ because
-        // the NotificationChannel class is new and not in the support library
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            val name = "Fun Notifications"
-            val descriptionText = "All Msgs from a great autotune voiced dude"
-            val importance = NotificationManager.IMPORTANCE_DEFAULT
-            val channel = NotificationChannel(FUN_CHANNEL_ID, name, importance).apply {
-                description = descriptionText
-            }
-            notificationManagerCompat.createNotificationChannel(channel)
-        }
-    }
+
 
     companion object {
         const val FUN_CHANNEL_ID = "FUNCHANNELID"
     }
+
+
 
 }
--- app/src/main/java/com/jhoupps/annoyingex/MainActivity.kt
+++ app/src/main/java/com/jhoupps/annoyingex/MainActivity.kt
@@ -2,6 +2,7 @@ package com.jhoupps.annoyingex
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -22,6 +23,10 @@ class MainActivity : AppCompatActivity() {
             exApp.exNotificationManager.postItNote() //todo apparently we should use poking the bear manager here
         }
 
+        btnCancel.setOnClickListener{
+            manageTheWorkManagerManager.stopWork()
+            Toast.makeText(this, "Successfully Blocked \"That F@#%ing B@st@rd\"",Toast.LENGTH_SHORT ).show()
+        }
 
         //test data form until I get the api set up
         //TODO - CALL THIS FROM THE INTERNET
